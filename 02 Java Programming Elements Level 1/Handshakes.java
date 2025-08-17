4a832fe33e32d2702f1502a3e17e1ad11beb4454
Shivam Kumaer <sk7220068@gmail.com>
2025-08-17 11:47:29 +0530

    first commit

---
+++ 02 Java Programming Elements Level 1/Handshakes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Handshakes {
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = input.nextInt();
+        
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+        
+        System.out.println("The maximum number of possible handshakes is " + handshakes);
+    }
+
+}
