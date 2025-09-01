cf56e9742817fef4c38eeb0c6db0bbfbcf17d5d6
Shivam Kumaer <sk7220068@gmail.com>
2025-09-01 15:49:24 +0530

    first commit

--- Day1_ProgrammingElements/02 Java Programming Elements Level 1/AverageMarks.java
+++
@@ -1,8 +0,0 @@
-public class AverageMarks {
-	public static void main(String[] args) {
-		int maths = 94, physics = 95, chemistry = 96;
-		int average = (maths+physics+chemistry)/3;
-		System.out.println("Sam's average marks in PCM is:" + average);
-	}
-
-}
