8c63ca22097db10a554fc0c764895655e9dda2aa
Shivam Kumaer <sk7220068@gmail.com>
2025-09-15 17:02:08 +0530

    Palindrome

---
+++ Java_Class_And_Object/Level2/Palindrome.java
@@ -0,0 +1,41 @@
+class PalindromeChecker {
+    String text;
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    boolean isPalindrome() {
+        String cleaned = text.replaceAll("\\s+", "").toLowerCase();
+        int i = 0, j = cleaned.length() - 1;
+
+        while (i < j) {
+            if (cleaned.charAt(i) != cleaned.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+}
+
+public class Palindrome {
+    public static void main(String[] args) {
+        PalindromeChecker p1 = new PalindromeChecker("madam");
+        PalindromeChecker p2 = new PalindromeChecker("hello");
+        PalindromeChecker p3 = new PalindromeChecker("Race car");
+
+        p1.displayResult();
+        p2.displayResult();
+        p3.displayResult();
+    }
+}
